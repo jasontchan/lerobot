some normalization fixes for emg

lerobot/common/datasets/factory.py
@@ -24,6 +24,7 @@ from lerobot.common.datasets.lerobot_dataset import (
     MultiLeRobotDataset,
 )
 from lerobot.common.datasets.transforms import ImageTransforms
+from lerobot.common.datasets.transforms import RunningNormalizer
 from lerobot.configs.policies import PreTrainedConfig
 from lerobot.configs.train import TrainPipelineConfig
 
@@ -58,7 +59,9 @@ def resolve_delta_timestamps(
         if key == "action" and cfg.action_delta_indices is not None:
             delta_timestamps[key] = [i / ds_meta.fps for i in cfg.action_delta_indices]
         if key.startswith("observation.") and cfg.observation_delta_indices is not None:
-            delta_timestamps[key] = [i / ds_meta.fps for i in cfg.observation_delta_indices]
+            delta_timestamps[key] = [
+                i / ds_meta.fps for i in cfg.observation_delta_indices
+            ]
 
     if len(delta_timestamps) == 0:
         delta_timestamps = None
@@ -79,8 +82,14 @@ def make_dataset(cfg: TrainPipelineConfig) -> LeRobotDataset | MultiLeRobotDatas
         LeRobotDataset | MultiLeRobotDataset
     """
     image_transforms = (
-        ImageTransforms(cfg.dataset.image_transforms) if cfg.dataset.image_transforms.enable else None
+        ImageTransforms(cfg.dataset.image_transforms)
+        if cfg.dataset.image_transforms.enable
+        else None
     )
+
+    # emg_normalizer = (
+    #     RunningNormalizer(n_channels=8) if cfg.use_emg and cfg.normalize_emg else None
+    # )
 
     if isinstance(cfg.dataset.repo_id, str):
         ds_meta = LeRobotDatasetMetadata(
@@ -93,6 +102,7 @@ def make_dataset(cfg: TrainPipelineConfig) -> LeRobotDataset | MultiLeRobotDatas
             episodes=cfg.dataset.episodes,
             delta_timestamps=delta_timestamps,
             image_transforms=image_transforms,
+            # emg_normalizer=emg_normalizer,
             revision=cfg.dataset.revision,
             video_backend=cfg.dataset.video_backend,
         )
@@ -113,6 +123,8 @@ def make_dataset(cfg: TrainPipelineConfig) -> LeRobotDataset | MultiLeRobotDatas
     if cfg.dataset.use_imagenet_stats:
         for key in dataset.meta.camera_keys:
             for stats_type, stats in IMAGENET_STATS.items():
-                dataset.meta.stats[key][stats_type] = torch.tensor(stats, dtype=torch.float32)
+                dataset.meta.stats[key][stats_type] = torch.tensor(
+                    stats, dtype=torch.float32
+                )
 
     return dataset

lerobot/common/datasets/transforms.py
@@ -23,6 +23,42 @@ from torchvision.transforms.v2 import Transform
 from torchvision.transforms.v2 import functional as F  # noqa: N812
 
 
+# class RunningNormalizer:
+#     """A running normalizer that computes the mean and variance of input data over time.
+#     Specifically, this is used to normalize EMG data."""
+
+#     def __init__(self, n_channels, momentum=0.01, eps=1e-6):
+#         self.n_channels = n_channels
+#         self.momentum = momentum
+#         self.eps = eps
+#         self.registered = False
+
+#     def _init_stats(self, x):
+#         mean = x.mean(dim=0)
+#         var = x.var(dim=0, unbiased=False)
+#         self.mean = mean.clone()
+#         self.var = var.clone()
+#         self.registered = True
+
+#     def update(self, x):
+#         # x shape: (time, channels)
+#         batch_mean = x.mean(dim=0)  # (channels,)
+#         batch_var = x.var(dim=0, unbiased=False)
+
+#         self.mean = (1 - self.momentum) * self.mean + self.momentum * batch_mean
+#         self.var = (1 - self.momentum) * self.var + self.momentum * batch_var
+
+#     def normalize(self, x):
+#         # normalize along channel dimension
+#         return (x - self.mean[None, :]) / (torch.sqrt(self.var[None, :]) + self.eps)
+
+#     def __call__(self, x):
+#         if not self.registered:
+#             self._init_stats(x)
+#         self.update(x)
+#         return self.normalize(x)
+
+
 class RandomSubsetApply(Transform):
     """Apply a random subset of N transformations from a list of transformations.
 
@@ -58,7 +94,9 @@ class RandomSubsetApply(Transform):
         elif not isinstance(n_subset, int):
             raise TypeError("n_subset should be an int or None")
         elif not (1 <= n_subset <= len(transforms)):
-            raise ValueError(f"n_subset should be in the interval [1, {len(transforms)}]")
+            raise ValueError(
+                f"n_subset should be in the interval [1, {len(transforms)}]"
+            )
 
         self.transforms = transforms
         total = sum(p)
@@ -119,26 +157,36 @@ class SharpnessJitter(Transform):
     def _check_input(self, sharpness):
         if isinstance(sharpness, (int, float)):
             if sharpness < 0:
-                raise ValueError("If sharpness is a single number, it must be non negative.")
+                raise ValueError(
+                    "If sharpness is a single number, it must be non negative."
+                )
             sharpness = [1.0 - sharpness, 1.0 + sharpness]
             sharpness[0] = max(sharpness[0], 0.0)
         elif isinstance(sharpness, collections.abc.Sequence) and len(sharpness) == 2:
             sharpness = [float(v) for v in sharpness]
         else:
-            raise TypeError(f"{sharpness=} should be a single number or a sequence with length 2.")
+            raise TypeError(
+                f"{sharpness=} should be a single number or a sequence with length 2."
+            )
 
         if not 0.0 <= sharpness[0] <= sharpness[1]:
-            raise ValueError(f"sharpness values should be between (0., inf), but got {sharpness}.")
+            raise ValueError(
+                f"sharpness values should be between (0., inf), but got {sharpness}."
+            )
 
         return float(sharpness[0]), float(sharpness[1])
 
     def make_params(self, flat_inputs: list[Any]) -> dict[str, Any]:
-        sharpness_factor = torch.empty(1).uniform_(self.sharpness[0], self.sharpness[1]).item()
+        sharpness_factor = (
+            torch.empty(1).uniform_(self.sharpness[0], self.sharpness[1]).item()
+        )
         return {"sharpness_factor": sharpness_factor}
 
     def transform(self, inpt: Any, params: dict[str, Any]) -> Any:
         sharpness_factor = params["sharpness_factor"]
-        return self._call_kernel(F.adjust_sharpness, inpt, sharpness_factor=sharpness_factor)
+        return self._call_kernel(
+            F.adjust_sharpness, inpt, sharpness_factor=sharpness_factor
+        )
 
 
 @dataclass

lerobot/common/policies/normalize.py
@@ -53,6 +53,11 @@ def create_stats_buffers(
             assert c < h and c < w, f"{key} is not channel first ({shape=})"
             # override image shape to be invariant to height and width
             shape = (c, 1, 1)
+        elif ft.type is FeatureType.EMG:
+            assert len(shape) == 2, f"number of dimensions of {key} != 2 ({shape=})"
+            t, c = shape
+            assert c < t, f"{key} is not channel first ({shape=})"
+            shape = (c, 1)
 
         # Note: we initialize mean, std, min, max to infinity. They should be overwritten
         # downstream by `stats` or `policy.load_state_dict`, as expected. During forward,
@@ -82,25 +87,43 @@ def create_stats_buffers(
         if stats:
             if isinstance(stats[key]["mean"], np.ndarray):
                 if norm_mode is NormalizationMode.MEAN_STD:
-                    buffer["mean"].data = torch.from_numpy(stats[key]["mean"]).to(dtype=torch.float32)
-                    buffer["std"].data = torch.from_numpy(stats[key]["std"]).to(dtype=torch.float32)
+                    buffer["mean"].data = torch.from_numpy(stats[key]["mean"]).to(
+                        dtype=torch.float32
+                    )
+                    buffer["std"].data = torch.from_numpy(stats[key]["std"]).to(
+                        dtype=torch.float32
+                    )
                 elif norm_mode is NormalizationMode.MIN_MAX:
-                    buffer["min"].data = torch.from_numpy(stats[key]["min"]).to(dtype=torch.float32)
-                    buffer["max"].data = torch.from_numpy(stats[key]["max"]).to(dtype=torch.float32)
+                    buffer["min"].data = torch.from_numpy(stats[key]["min"]).to(
+                        dtype=torch.float32
+                    )
+                    buffer["max"].data = torch.from_numpy(stats[key]["max"]).to(
+                        dtype=torch.float32
+                    )
             elif isinstance(stats[key]["mean"], torch.Tensor):
                 # Note: The clone is needed to make sure that the logic in save_pretrained doesn't see duplicated
                 # tensors anywhere (for example, when we use the same stats for normalization and
                 # unnormalization). See the logic here
                 # https://github.com/huggingface/safetensors/blob/079781fd0dc455ba0fe851e2b4507c33d0c0d407/bindings/python/py_src/safetensors/torch.py#L97.
                 if norm_mode is NormalizationMode.MEAN_STD:
-                    buffer["mean"].data = stats[key]["mean"].clone().to(dtype=torch.float32)
-                    buffer["std"].data = stats[key]["std"].clone().to(dtype=torch.float32)
+                    buffer["mean"].data = (
+                        stats[key]["mean"].clone().to(dtype=torch.float32)
+                    )
+                    buffer["std"].data = (
+                        stats[key]["std"].clone().to(dtype=torch.float32)
+                    )
                 elif norm_mode is NormalizationMode.MIN_MAX:
-                    buffer["min"].data = stats[key]["min"].clone().to(dtype=torch.float32)
-                    buffer["max"].data = stats[key]["max"].clone().to(dtype=torch.float32)
+                    buffer["min"].data = (
+                        stats[key]["min"].clone().to(dtype=torch.float32)
+                    )
+                    buffer["max"].data = (
+                        stats[key]["max"].clone().to(dtype=torch.float32)
+                    )
             else:
                 type_ = type(stats[key]["mean"])
-                raise ValueError(f"np.ndarray or torch.Tensor expected, but type is '{type_}' instead.")
+                raise ValueError(
+                    f"np.ndarray or torch.Tensor expected, but type is '{type_}' instead."
+                )
 
         stats_buffers[key] = buffer
     return stats_buffers
@@ -278,6 +301,10 @@ def _initialize_stats_buffers(
             # reduce spatial dimensions, keep channel dimension only
             c, *_ = shape
             shape = (c, 1, 1)
+        elif ft.type is FeatureType.EMG:
+            # reduce temporal dimension, keep channel dimension only
+            c, *_ = shape
+            shape = (c, 1)
 
         prefix = key.replace(".", "_")
 
@@ -296,7 +323,9 @@ def _initialize_stats_buffers(
                     mean = mean_data.clone().to(dtype=torch.float32)
                     std = std_data.clone().to(dtype=torch.float32)
                 else:
-                    raise ValueError(f"Unsupported stats type for key '{key}' (expected ndarray or Tensor).")
+                    raise ValueError(
+                        f"Unsupported stats type for key '{key}' (expected ndarray or Tensor)."
+                    )
 
             module.register_buffer(f"{prefix}_mean", mean)
             module.register_buffer(f"{prefix}_std", std)
@@ -313,7 +342,9 @@ def _initialize_stats_buffers(
                     min_val = min_data.clone().to(dtype=torch.float32)
                     max_val = max_data.clone().to(dtype=torch.float32)
                 else:
-                    raise ValueError(f"Unsupported stats type for key '{key}' (expected ndarray or Tensor).")
+                    raise ValueError(
+                        f"Unsupported stats type for key '{key}' (expected ndarray or Tensor)."
+                    )
 
             module.register_buffer(f"{prefix}_min", min_val)
             module.register_buffer(f"{prefix}_max", max_val)

lerobot/common/policies/smolvla/modeling_smolvla.py
@@ -57,6 +57,8 @@ import os
 import re
 from collections import deque
 
+from matplotlib import pyplot as plt
+import numpy as np
 import safetensors
 import torchaudio
 import torch
@@ -77,8 +79,24 @@ from lerobot.common.policies.utils import (
 )
 from lerobot.common.utils.utils import get_safe_dtype
 
+# import pyqtgraph as pg
+# from pyqtgraph.Qt import QtWidgets
+# from sklearn.manifold import TSNE
+
 # Matches ".soNNN", optionally followed by "-something", up to the "_buffer_" marker
 _VARIANT_RE = re.compile(r"\.so\d+(?:-[\w]+)?_buffer_")
+
+
+# app = QtWidgets.QApplication([])
+# win = pg.GraphicsLayoutWidget(show=True, title="Real-Time Spectrogram")
+# view = win.addViewBox()
+# view.setAspectLocked()
+# img = pg.ImageItem()
+# view.addItem(img)
+
+
+# def update_spectrogram(spec_tensor):
+#     img.setImage(spec_tensor, autoLevels=False)
 
 
 def canonicalise(k: str) -> str:
@@ -627,6 +645,14 @@ class SmolVLAPolicy(PreTrainedPolicy):
                     ],
                     dim=1,
                 )  # (B, C, F, T)
+                # spec_np = emgs[0, 0, :, :].detach().cpu().numpy()
+                # spec_norm = (
+                #     255
+                #     * (spec_np - spec_np.min())
+                #     / (spec_np.max() - spec_np.min() + 1e-6)
+                # ).astype(np.uint8)
+                # update_spectrogram(spec_norm)
+                # QtWidgets.QApplication.processEvents()
         else:
             for key in present_emg_keys:
                 append_emg = batch[key][:, -1, :] if batch[key].ndim > 2 else batch[key]
@@ -748,7 +774,7 @@ class VLAFlowMatching(nn.Module):
                 padding=1,
             ),
             nn.ReLU(),
-            nn.MaxPool2d(2, 1), #pool freq only, keep time
+            nn.MaxPool2d(2, 1),  # pool freq only, keep time TEMP: (2, 1)
             nn.Conv2d(
                 in_channels=32,
                 out_channels=64,
@@ -756,11 +782,15 @@ class VLAFlowMatching(nn.Module):
                 padding=1,
             ),
             nn.ReLU(),
-            nn.AdaptiveAvgPool2d((1, 3)),  # 3 time steps back
-            # nn.Flatten(),
-            # nn.Linear(64, self.vlm_with_expert.config.text_config.hidden_size),
+            nn.AdaptiveAvgPool2d((1, 3)),  # 3 time steps back #TEMP: 1, 3
+            # nn.Flatten(),  # TEMP: comment out
+            # nn.Linear(
+            #     64, self.vlm_with_expert.config.text_config.hidden_size
+            # ),  # TEMP: comment out
         )
-        self.cnn_proj = nn.Linear(64, self.vlm_with_expert.config.text_config.hidden_size)
+        self.cnn_proj = nn.Linear(
+            64, self.vlm_with_expert.config.text_config.hidden_size
+        )  # TEMP: include this
 
         self.set_requires_grad()
         self.fake_image_token = (
@@ -890,12 +920,15 @@ class VLAFlowMatching(nn.Module):
         if emg is not None:
             if self.config.emg_spectrogram:
                 emg_emb = self.emg_cnn(emg)
-                emg_emb = emg_emb.squeeze(2).permute(0, 2, 1) #(B, out_time_steps, 64)
-                emg_emb = self.cnn_proj(emg_emb)  # (B, out_time_steps, hidden_size)
-                emg_emb = emg_emb[:, None, :] if emg_emb.ndim == 2 else emg_emb
+                emg_emb = emg_emb.squeeze(2).permute(
+                    0, 2, 1
+                )  # (B, out_time_steps, 64) #FOR TEMP
+                emg_emb = self.cnn_proj(
+                    emg_emb
+                )  # (B, out_time_steps, hidden_size) #FOR TEMP
             else:
                 emg_emb = self.emg_proj(emg)
-                emg_emb = emg_emb[:, None, :] if emg_emb.ndim == 2 else emg_emb
+            emg_emb = emg_emb[:, None, :] if emg_emb.ndim == 2 else emg_emb
             embs.append(emg_emb)
             bsize_emg = emg_emb.shape[0]
             device_emg = emg_emb.device